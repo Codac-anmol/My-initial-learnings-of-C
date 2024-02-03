34857a3f2d094f998e67e884b7a6e142847ac58c
Anmol Raj <140152011+Codac-anmol@users.noreply.github.com>
2024-02-03 06:54:33 +0000

    added sub and fixed first input out of loop

--- Calculator.c
+++ Calculator.c
@@ -2,26 +2,30 @@
 #include<math.h> 
 #include<string.h> 
 float add(float j); 
-float subtract(); 
+float subtract(float j); 
 float multiply(); 
 float divide(); 
 int main(){ 
     float box; 
     char operat = '+'; 
     char exitcontrol= 'c'; 
-   do 
-   { 
-     printf("enter the first no for peration"); 
-    scanf("%f",& box); 
+    printf("welcome to calcuminia by anmol\n");
+    printf("you can use operators like :\n = :to see result right now \n +:to add \n -:to subtract \n *:to multiply \n /:to devide \n");
+    printf("enter the first no for operation"); 
+    scanf("%f",& box);
+    do{
     printf("enter operator"); 
     scanf(" %c", &operat); 
     if (operat == '+') 
     { 
        box = add(box);
-       
+       printf(" want to operate more enter c else to getresult and exit press x ");
+       scanf(" %c",&exitcontrol);
     } 
+
      
    } while (exitcontrol == 'c' ); 
+   printf("the final value is %f ",box);
    return 0; 
 } 
 float add(float j){ 
@@ -30,4 +34,11 @@ float add(float j){
     scanf("%f",&k); 
     sum=j+k; 
     return sum; 
+}
+float subtract(float j){ 
+    float k,sub; 
+    printf("enter the no you want to subtract"); 
+    scanf("%f",&k); 
+    sub=j-k; 
+    return sub; 
 }
