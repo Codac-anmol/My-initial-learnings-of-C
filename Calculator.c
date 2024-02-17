6ea430b076be8f8d04769711c16c0dd29d2d67a7
Anmol Raj <140152011+Codac-anmol@users.noreply.github.com>
2024-02-17 17:48:18 +0000

    modified

--- Calculator.c
+++ Calculator.c
@@ -3,14 +3,14 @@
 #include<string.h> 
 float add(float j); 
 float subtract(float j); 
-float multiply(); 
-float divide(); 
+float multiply(float j); 
+float divide(float j);
 int main(){ 
     float box; 
     char operat = '+'; 
     char exitcontrol= 'c'; 
     printf("welcome to calcuminia by anmol\n");
-    printf("you can use operators like :\n = :to see result right now \n +:to add \n -:to subtract \n *:to multiply \n /:to devide \n");
+    printf("you can use operators like :\n = :to see result right now \n +:to add \n -:to subtract \n *:to multiply \n /:to devide \n x:to close program immediately \n ");
     printf("enter the first no for operation"); 
     scanf("%f",& box);
     do{
@@ -22,8 +22,34 @@ int main(){
        printf(" want to operate more enter c else to getresult and exit press x ");
        scanf(" %c",&exitcontrol);
     } 
-
-     
+    
+    if (operat == '-') 
+    { 
+       box = subtract(box);
+       printf(" want to operate more enter c else to getresult and exit press x ");
+       scanf(" %c",&exitcontrol);
+    } 
+    
+     if (operat == '*') 
+    { 
+       box = multiply(box);
+       printf(" want to operate more enter c else to getresult and exit press x ");
+       scanf(" %c",&exitcontrol);
+    }
+     if (operat == '/') 
+    { 
+       box = divide(box);
+       printf(" want to operate more enter c else to getresult and exit press x ");
+       scanf(" %c",&exitcontrol);
+    }
+    if ( operat == '='){
+        printf("your value right now is %f ",box);
+        printf("please enter next operator to calculate or c to exit ");
+        scanf("%c",&operat);
+    }
+    if ( operat == 'c'){
+        exitcontrol=='x';
+    }
    } while (exitcontrol == 'c' ); 
    printf("the final value is %f ",box);
    return 0; 
@@ -41,4 +67,24 @@ float subtract(float j){
     scanf("%f",&k); 
     sub=j-k; 
     return sub; 
+}
+float multiply(float j){ 
+    float k,sub; 
+    printf("enter the no you want to multiply "); 
+    scanf("%f",&k); 
+    sub=j*k; 
+    return sub; 
+}
+float divide(float j){ 
+    float k,sub; 
+    printf("enter the no you want to divide %f from ",j); 
+    scanf("%f",&k);
+    if(k==0){
+        printf("any no divided by 0 is undefined please try again ");
+        return j;
+    }
+    else{
+        sub=j/k; 
+        return sub; 
+    }
 }
